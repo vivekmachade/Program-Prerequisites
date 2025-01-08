b84e904d79515fe3bcf475628e2e62faa58cc65e
Vivek Machade <vivekmachade323@gmail.com>
2025-01-08 11:02:12 +0530

    Question 2: Sum of two number

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+System.out.println("write the value a : ");
+int a = scanner.nextInt();
+System.out.println("write the value b : ");
+int b = scanner.nextInt();
+int c = a+b;
+System.out.println("Total value : "+c);
+}
+}
