7541f727a4fb49a94a5cc2f5178a38f85bb0099c
Vivek Machade <vivekmachade323@gmail.com>
2025-01-08 11:15:55 +0530

    Question 10: Convert Kilometer to Miles

---
+++ KiloToMiles.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class KiloToMiles{
+public static void main(String[] args){
+
+
+Scanner scanner = new Scanner(System.in);
+System.out.print("write the kilometers : ");
+double kilo = scanner.nextInt();
+
+double miles = kilo*0.621371;
+System.out.println("kilometer to miles :"+miles);
+}
+}
