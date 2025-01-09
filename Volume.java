f40d3a701b07ea1c05070b2e2100f90fb2635f02
vivekmachade <101501915+vivekmachade@users.noreply.github.com>
2025-01-09 11:34:19 +0530

    Update Volume.java

--- Volume.java
+++ Volume.java
@@ -6,10 +6,10 @@ Scanner scanner = new Scanner(System.in);
 System.out.println("What is radius of circle :");
 
 double pie = 3.141;
-double radius = scanner.nextdouble();
+double radius = scanner.nextDouble();
 
 System.out.print("what is your height");
-double height=scanner.nextdouble();
+double height=scanner.nextDouble();
 double volume = pie * radius*radius*height;
 
 System.out.println("volume of cylinder : "+volume);
