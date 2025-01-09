23690b4d9b51cf423cf726264e7793c88dfe5a61
vivekmachade <101501915+vivekmachade@users.noreply.github.com>
2025-01-09 11:06:41 +0530

    Update KiloToMiles.java

--- KiloToMiles.java
+++ KiloToMiles.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
 
 Scanner scanner = new Scanner(System.in);
 System.out.print("write the kilometers : ");
-double kilo = scanner.nextInt();
+double kilo = scanner.nextdoulbe();
 
 double miles = kilo*0.621371;
 System.out.println("kilometer to miles :"+miles);
