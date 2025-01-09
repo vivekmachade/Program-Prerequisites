debaf745ab98c25fac62f3ffcd79fc733688c865
vivekmachade <101501915+vivekmachade@users.noreply.github.com>
2025-01-09 11:37:50 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -6,7 +6,7 @@ Scanner scanner = new Scanner(System.in);
 System.out.println("What is radius of circle :");
 
 double pie = 3.141;
-double radius = scanner.nextdouble();
+double radius = scanner.nextDouble();
 double area = pie * radius*radius;
 
 System.out.println("Area of circle : "+area);
